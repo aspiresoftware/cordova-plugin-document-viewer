fac58609863e6e5853d0b844641363dedea67d1f
Ramesh Sharma <ramesh.sharam@aspiresoftware.in>
2017-04-12 10:14:34 +0530

    fix change for classname exception

--- src/android/de/sitewaerts/cordova/documentviewer/DocumentViewerPlugin.java
+++ src/android/de/sitewaerts/cordova/documentviewer/DocumentViewerPlugin.java
@@ -21,7 +21,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-package be.sitewaerts.cordova.documentviewer;
+package de.sitewaerts.cordova.documentviewer;
 
 import android.content.ComponentName;
 import android.content.Intent;
